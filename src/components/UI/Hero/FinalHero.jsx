a58f416741e43c440698b409b50aa191364b2d16
Abdullah-5603 <abdullah72308@gmail.com>
2024-10-20 21:54:31 +0600

    Responsive optimized in Hero section

--- src/components/UI/Hero/FinalHero.jsx
+++ src/components/UI/Hero/FinalHero.jsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 
 const FinalHero = () => {
     return (
-        <div className=' h-full pb-10 md:pb-4 lg:pb-14 pt-24 relative mb-16'>
+        <div className='bg-custom-gradient lg:bg-none h-full pb-10 md:pb-4 lg:pb-14 pt-24 relative mb-16'>
 
-            <div className='absolute w-full h-full top-0'>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600" version="1.1">
+            <div className='hidden lg:block absolute  w-full h-full top-0'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 400" version="1.1">
                     <defs>
                         <linearGradient id="gradient1" x1="0%" y1="0%" x2="0%" y2="100%">
                             <stop offset="0%" style={{ stopColor: 'rgba(0,0,36,1)', stopOpacity: 1 }} />
@@ -60,20 +60,17 @@ const FinalHero = () => {
             </div>
 
             {/* Bottom Image */}
-            {/* <div className='absolute xl:top[3in] lg:top-[3.5in] md:top-[3.4in] sm:bottom-0 bottom-0 w-full z-0'>
+            <div className='absolute lg:hidden -bottom-1'>
                 <Image 
                     src={HomeBottom}
                     width={500}
                     height={500}
-                    className='max-w-screen'
+                    className='w-screen'
                     alt="Bottom shape image"
                 />
-            </div> */}
+            </div>
         </div>
     );
 };
 
 export default FinalHero;
-
-
-{/* <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="visual" viewBox="0 0 900 600" width="900" height="600" version="1.1"><path d="M0 437L25 419.5C50 402 100 367 150 358C200 349 250 366 300 373.2C350 380.3 400 377.7 450 339C500 300.3 550 225.7 600 207.2C650 188.7 700 226.3 750 236.3C800 246.3 850 228.7 875 219.8L900 211L900 0L875 0C850 0 800 0 750 0C700 0 650 0 600 0C550 0 500 0 450 0C400 0 350 0 300 0C250 0 200 0 150 0C100 0 50 0 25 0L0 0Z" fill="#0066FF" stroke-linecap="round" stroke-linejoin="miter"/></svg> */ }
--- src/components/UI/Testimonials/Testimonials.jsx
+++ src/components/UI/Testimonials/Testimonials.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import {
-    Carousel,
-    CarouselButtons,
-    CarouselControl,
-    CarouselItem,
-    CarouselNextButton,
-    CarouselPrevButton,
-    CarouselSlides,
-} from 'keep-react';
+
 import Image from 'next/image';
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+// import { Carousel } from 'react-responsive-carousel';
 
 import image1 from '../../../assets/client-review-image/c87497aa55_M.rony_.png';
 import image2 from '../../../assets/client-review-image/Frame-39970.png';
@@ -18,106 +12,32 @@ import '@smastrom/react-rating/style.css'
 
 const Testimonials = () => {
     return (
-        <div className="mt-10">
+        <div className="mt-10 w-full md:w-4/5 mx-auto">
             {/* Heading Section */}
             <div className="my-5 text-center">
-                <h2 className="lg:text-4xl md:text-3xl sm:text-2xl text-2xl py-2 relative uppercase">
-                    Testimonials From Our Clients
-                    <span className="block w-14 h-1 bg-[#4a3989] absolute bottom-0 left-1/2 transform -translate-x-1/2 mt-1"></span>
+                <h2 className="text-[var(--color-secondary)] font-semibold text-lg py-2 relative uppercase">
+                    our reviews
                 </h2>
-                <p className="lg:text-xl md:text-xl sm:text-xs text-xs text-[#4a3989] mt-3">
-                    GET IN TOUCH
+                <p className="text-xl  text-[#4a3989] mt-3">
+                    Client Feedback
                 </p>
             </div>
 
             {/* Carousel Section */}
-            <Carousel options={{ slidesToScroll: 1, slidesToShow: 2 }}>
-                <CarouselSlides className="flex gap-5">
-                    {/* Slider 1 */}
-                    <CarouselItem className="flex-[0_0 100%] lg:flex-[0_0_33%]">
-                        <div className="rounded-lg overflow-hidden shadow-lg h-full cursor-pointer p-5">
-                            <div className="flex items-center justify-center mb-4">
-                                <Image
-                                    src={image1}
-                                    alt="Web Development"
-                                    className="rounded-full shadow-lg"
-                                    width={120}
-                                    height={120}
-                                />
-                            </div>
-                            <div className="px-4 text-center">
-                             <div className='flex justify-center my-2'>
-                             <Rating
-                                    style={{ maxWidth: 180 }}
-                                    value={5}
-                                    readOnly
-                                />
-                             </div>
-                                <p className="text-gray-600 text-base leading-relaxed">
-                                    Monezsoft is a Marketing Agency. We are outsourcing and collaborating with workspace infotech limited.
-                                </p>
-                            </div>
-                        </div>
-                    </CarouselItem>
-
-                    {/* Slider 2 */}
-                    <CarouselItem className="flex-[0_0 100%] lg:flex-[0_0_33%]">
-                        <div className="rounded-lg overflow-hidden shadow-lg h-full cursor-pointer p-5">
-                            <div className="flex items-center justify-center mb-4">
-                                <Image
-                                    src={image2}
-                                    alt="WordPress Development"
-                                    className="rounded-full shadow-lg"
-                                    width={120}
-                                    height={120}
-                                />
-                            </div>
-                            <div className="px-4 text-center">
-                            <div className='flex justify-center my-2'>
-                             <Rating
-                                    style={{ maxWidth: 180 }}
-                                    value={4}
-                                    readOnly
-                                />
-                             </div>
-                                <p className="text-gray-600 text-base leading-relaxed">
-                                    I remember, I found WSIT five years back with the skills and experience I needed to build Flowcite. Since then they have been working on this project as they own it.
-                                </p>
-                            </div>
-                        </div>
-                    </CarouselItem>
-
-                    {/* Slider 3 */}
-                    <CarouselItem className="flex-[0_0 100%] lg:flex-[0_0_33%]">
-                        <div className="rounded-lg overflow-hidden shadow-lg h-full cursor-pointer p-5">
-                            <div className="flex items-center justify-center mb-4">
-                                <Image
-                                    src={image3}
-                                    alt="E-Commerce Development"
-                                    className="rounded-full shadow-lg"
-                                    width={120}
-                                    height={120}
-                                />
-                            </div>
-                            <div className="px-4 text-center">
-                            <div className='flex justify-center my-2'>
-                             <Rating
-                                    style={{ maxWidth: 180 }}
-                                    value={4.5}
-                                    readOnly
-                                />
-                             </div>
-                                <p className="text-gray-600 text-base leading-relaxed">
-                                    Tbh I enjoyed working with WorskapceIT and Tahsin. Best wishes for them.
-                                </p>
-                            </div>
-                        </div>
-                    </CarouselItem>
-                </CarouselSlides>
-
-                
-             
-            </Carousel>
+            {/* <Carousel>
+                <div>
+                    <img src={image1} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={image2} />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src={image3} />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel> */}
         </div>
     );
 };
