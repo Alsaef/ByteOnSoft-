191bb194fc649835d27c3b9cf5925cfaab6db5df
Abdullah-5603 <abdullah72308@gmail.com>
2024-10-21 12:09:50 +0600

    Team member section added in home page

--- src/components/UI/OurClient/OurClient.jsx
+++ src/components/UI/OurClient/OurClient.jsx
@@ -9,116 +9,115 @@ import image6 from '../../../assets/client image/client-6.png';
 import image7 from '../../../assets/client image/client-7.png';
 import image8 from '../../../assets/client image/client-8.png';
 import image9 from '../../../assets/client image/client-9.png';
+import SectionTitle from '@/components/SectionTitle/SectionTitle';
 
 const OurClient = () => {
     return (
-        <div className='mt-10 w-full md:w-4/5 mx-auto'>
+        <div className='mt-16 w-full md:w-4/5 mx-auto'>
             {/* Title Section */}
-            <div className='my-5'>
-                <p className='uppercase text-center text-[var(--color-secondary)] py-2 text-lg font-semibold'>Trusted By Over 1500</p>
-                <h2 className="text-2xl md:text-3xl text-center py-2 font-bold text-[#404040]">
-                    OUR CLIENTS
-                </h2>
-            </div>
+            <SectionTitle
+                heading={'OUR CLIENTS'}
+                subHeading={'Trusted By Over 1500'}
+            />
 
             {/* Image Grid Section */}
             <div className="flex flex-wrap justify-center gap-3 px-6">
                 {/* Image 1 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image1} 
-                        alt="Client 1" 
-                        width={150}          
-                        height={150}         
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                    <Image
+                        src={image1}
+                        alt="Client 1"
+                        width={150}
+                        height={150}
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 2 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image5} 
-                        alt="Client 5" 
+                    <Image
+                        src={image5}
+                        alt="Client 5"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 3 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image3} 
-                        alt="Client 3" 
+                    <Image
+                        src={image3}
+                        alt="Client 3"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 4 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image4} 
-                        alt="Client 4" 
+                    <Image
+                        src={image4}
+                        alt="Client 4"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 5 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image2} 
-                        alt="Client 2" 
+                    <Image
+                        src={image2}
+                        alt="Client 2"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 6 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image6} 
-                        alt="Client 6" 
+                    <Image
+                        src={image6}
+                        alt="Client 6"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 7 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image7} 
-                        alt="Client 7" 
+                    <Image
+                        src={image7}
+                        alt="Client 7"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 8 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image8} 
-                        alt="Client 8" 
+                    <Image
+                        src={image8}
+                        alt="Client 8"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
 
                 {/* Image 9 */}
                 <div className="flex justify-center items-center">
-                    <Image 
-                        src={image9} 
-                        alt="Client 9" 
+                    <Image
+                        src={image9}
+                        alt="Client 9"
                         width={150}
                         height={150}
-                        className="rounded-lg border border-gray-300 shadow-lg " 
+                        className="rounded-lg border border-gray-300 shadow-lg "
                     />
                 </div>
             </div>
--- src/components/UI/Testimonials/Testimonials.jsx
+++ src/components/UI/Testimonials/Testimonials.jsx
@@ -10,26 +10,23 @@ import 'swiper/css';
 
 import image1 from '../../../assets/client-review-image/person-1.webp';
 import SwiperCard from '@/components/Card/SwiperCard';
+import SectionTitle from '@/components/SectionTitle/SectionTitle';
 
 const Testimonials = () => {
     return (
-        <div className="mt-10 w-11/12 md:w-4/5 mx-auto">
+        <div className="mt-16 w-11/12 md:w-4/5 mx-auto">
             {/* Heading Section */}
-            <div className="my-5 text-center">
-                <h2 className="text-[var(--color-secondary)] font-semibold text-lg py-2 relative uppercase">
-                    our reviews
-                </h2>
-                <p className="text-xl  text-[#4a3989] mt-3">
-                    Client Feedback
-                </p>
-            </div>
+            <SectionTitle
+            heading={'Our Testimonials'}
+            subHeading={'Client Feedbacks'}
+            />
 
             {/* Swiper Section */}
             <Swiper
-            modules={[Autoplay,Parallax]}
+                modules={[Autoplay, Parallax]}
                 spaceBetween={50}
                 slidesPerView={3}
-                autoplay={{ 
+                autoplay={{
                     delay: 3000, // delay between slides
                     pauseOnMouseEnter: true, // pause on hover
                 }}
@@ -53,57 +50,57 @@ const Testimonials = () => {
                 }}
             >
                 <SwiperSlide>
-                    <SwiperCard 
-                    image={image1} 
-                    name={'Jhon Doe'} 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
-                    location={'Baridhara, Dhaka'}
-                    rating={4}
+                    <SwiperCard
+                        image={image1}
+                        name={'Jhon Doe'}
+                        description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
+                        location={'Baridhara, Dhaka'}
+                        rating={4}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <SwiperCard 
-                    image={image1} 
-                    name={'Jhon Doe'} 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
-                    location={'Baridhara, Dhaka'}
-                    rating={4}
+                    <SwiperCard
+                        image={image1}
+                        name={'Jhon Doe'}
+                        description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
+                        location={'Baridhara, Dhaka'}
+                        rating={4}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <SwiperCard 
-                    image={image1} 
-                    name={'Jhon Doe'} 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
-                    location={'Baridhara, Dhaka'}
-                    rating={4}
+                    <SwiperCard
+                        image={image1}
+                        name={'Jhon Doe'}
+                        description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
+                        location={'Baridhara, Dhaka'}
+                        rating={4}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <SwiperCard 
-                    image={image1} 
-                    name={'Jhon Doe'} 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
-                    location={'Baridhara, Dhaka'}
-                    rating={4}
+                    <SwiperCard
+                        image={image1}
+                        name={'Jhon Doe'}
+                        description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
+                        location={'Baridhara, Dhaka'}
+                        rating={4}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <SwiperCard 
-                    image={image1} 
-                    name={'Jhon Doe'} 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
-                    location={'Baridhara, Dhaka'}
-                    rating={4}
+                    <SwiperCard
+                        image={image1}
+                        name={'Jhon Doe'}
+                        description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
+                        location={'Baridhara, Dhaka'}
+                        rating={4}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <SwiperCard 
-                    image={image1} 
-                    name={'Jhon Doe'} 
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
-                    location={'Baridhara, Dhaka'}
-                    rating={4}
+                    <SwiperCard
+                        image={image1}
+                        name={'Jhon Doe'}
+                        description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eveniet nemo veritatis, possimus quam aliquid voluptates sunt voluptatibus veniam. Suscipit dicta debitis ea quisquam neque reprehenderit porro libero harum. Eos?'}
+                        location={'Baridhara, Dhaka'}
+                        rating={4}
                     />
                 </SwiperSlide>
             </Swiper>
